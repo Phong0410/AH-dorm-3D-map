65589da017c5e24a44944ae564fa00fd1617a911
Phong0410 <66516738+Phong0410@users.noreply.github.com>
2021-12-20 22:24:12 +0700

    Add files via upload

---
+++ AH_data/loadblockdata.php
@@ -0,0 +1,43 @@
+<?php
+include_once 'config.php';
+
+header('Content-Type: application/json; charset=utf-8');
+error_reporting(E_ERROR | E_PARSE);
+
+$block;
+$conn = new mysqli($servername.":".$port, $username, $password, $dbname);
+
+$conn->set_charset("utf8");
+
+$body->type = "FeatureCollection";
+$body->generator = "smartcity";
+$body->copyright = "Smartcity";
+$body->timestamp = "2021-05-27T09:28:58Z";
+$body->features = [];
+
+$body_result = $conn->query("SELECT * FROM Body WHERE type='block'");
+
+while ($body_row = $body_result->fetch_assoc()) {
+    $object->type = "Feature";
+    $object->properties->Name = $body_row['NAME'];
+    $object->properties->height = intval($body_row['height']);
+    $object->properties->idb = $body_row['id_body'];
+    $object->properties->infor = $body_row['infor'];
+    $object->geometry->type = "Polygon";
+    $object->geometry->coordinates = [];
+    $object->id = $body_row['id_body'];
+    $array_node = [];
+
+    $node_result = $conn->query("SELECT * FROM ((Body_Face JOIN Face ON Body_Face.id_face = Face.id_Face) JOIN Face_Node ON Face.id_face = Face_Node.id_face) JOIN Node ON Node.id_node = Face_Node.id_node WHERE Body_Face.id_body = '" . $body_row['id_body'] . "' ORDER BY Face_Node.sequence");
+    while ($node_row = $node_result->fetch_assoc()) {
+        array_push($array_node, [floatval($node_row['x']), floatval($node_row['y']), floatval($node_row['z'])]);
+    }
+    array_push($object->geometry->coordinates, $array_node);
+    array_push($body->features, $object);
+    unset($object);
+}
+
+echo json_encode($body, JSON_UNESCAPED_UNICODE);
+
+$conn->close();
+?>
